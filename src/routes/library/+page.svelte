d6388ea74aab6b9b01cbe5205992ae9f72d6d383
Neal Wang <nealwang.sh@protonmail.com>
2026-02-14 00:30:23 -0800

    feat: library

---
+++ src/routes/library/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Banner from "$lib/components/Banner.svelte";
+    import Library from "./library.svx";
+</script>
+
+<Banner title="Library" image="wood" />
+
+<div class="mt-12 flex p-4 sm:p-8 md:justify-center">
+    <article class="prose max-w-none md:w-2/3 lg:prose-lg">
+        <Library />
+    </article>
+</div>
